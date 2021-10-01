9ee790cb43bf5869cd31247e8d310b3ec24da05f
Kirill Matveev <dejar@protonmail.com>
2021-10-01 18:20:52 +0300

    - Career page is ready - Update spoilers - Update breadcrumbs

--- src/assets/scripts/spoiler.js
+++ src/assets/scripts/spoiler.js
@@ -1,18 +1,10 @@
-import { event } from "jquery";
-
 {
   $(() => {
 
-    // const spoiler = $('.spoiler');
-    const spoilerBtn = $('.spoiler__button');
-    const spoilerDrop = $('.spoiler__dropdown');
-
-    // console.log(123);
-
-    spoilerBtn.on('click', event => {
+    $('.spoiler__button').on('click', event => {
       const currentSpoiler = $(event.target).closest('.spoiler');
       currentSpoiler.toggleClass('spoiler--active');
-      currentSpoiler.find(spoilerDrop).slideToggle(500);
+      currentSpoiler.find('.spoiler__dropdown').slideToggle(500);
     })
 
   })
