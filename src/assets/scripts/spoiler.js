ef3b57bb069ad3dcb4e4ac2d864a507a7ad8afaf
Kirill Matveev <dejar@protonmail.com>
2021-10-11 17:12:53 +0300

    Worked on fonts and pages adaptive

--- src/assets/scripts/spoiler.js
+++ src/assets/scripts/spoiler.js
@@ -1,11 +1,9 @@
 {
   $(() => {
-
     $('.spoiler__button').on('click', event => {
       const currentSpoiler = $(event.target).closest('.spoiler');
       currentSpoiler.toggleClass('spoiler--active');
       currentSpoiler.find('.spoiler__dropdown').slideToggle(500);
     })
-
   })
 }
