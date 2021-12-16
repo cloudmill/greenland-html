1008f31ea65561e299129ff53e6082465750363a
Nikita <nikita.ableev@mail.ru>
2021-12-16 13:15:41 +0300

    Start slider animation

--- src/assets/scripts/conditions-slider.js
+++ src/assets/scripts/conditions-slider.js
@@ -1,58 +1,60 @@
-import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
-Swiper.use([Navigation, Pagination, Mousewheel]);
 
-const conditions = [
-  {
-    "title": "с дизайнерами",
-    "bullet": "дизайнерам"
-  },
-  {
-    "title": "для объектов",
-    "bullet": "объектам"
-  },
-  {
-    "title": "для объектов",
-    "bullet": "дистрибьютерам и оптовым покупателям"
-  }
-]
 
-export default function initConditionsSlider() {
+// import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
+// Swiper.use([Navigation, Pagination, Mousewheel]);
 
-  const $conditionsSlider = document.querySelector('[data-conditions-slider]');
-  const $conditionsSliderBullets = $conditionsSlider.querySelector('[data-conditions-slider-bullets]');
+// const conditions = [
+//   {
+//     "title": "с дизайнерами",
+//     "bullet": "дизайнерам"
+//   },
+//   {
+//     "title": "для объектов",
+//     "bullet": "объектам"
+//   },
+//   {
+//     "title": "для объектов",
+//     "bullet": "дистрибьютерам и оптовым покупателям"
+//   }
+// ]
 
-  const conditionsSlider = new Swiper($conditionsSlider, {
-    mousewheel: {
-      releaseOnEdges: true
-    },
-    pagination: {
-      el: $conditionsSliderBullets,
-      clickable: true,
-      renderBullet: function(index, className) {
-        return `
-          <div class="${className} conditions__slider-bullet">
+// export default function initConditionsSlider() {
 
-            <div class="conditions__slider-bullet-line"></div>
-            <div class="conditions__slider-bullet-square">
-              <div class="conditions__slider-bullet-text text__md text--uppercase">
-                <div class="conditions__slider-bullet-text-number">${index + 1}</div>
-                ${conditions[index].bullet}</div>
-            </div>
-          </div>
-        `;
-      }
-    },
-    on: {
-      slideChangeTransitionEnd: function() {
-        if (this.isBeginning || this.isEnd) {
-          this.params.mousewheel.releaseOnEdges = true;
-        } else {
-          this.params.mousewheel.releaseOnEdges = false;
-        }
-      }
-    },
-    // slidesPerView: 1.2,
-  });
+//   const $conditionsSlider = document.querySelector('[data-conditions-slider]');
+//   const $conditionsSliderBullets = $conditionsSlider.querySelector('[data-conditions-slider-bullets]');
 
-  window.sliders.push(conditionsSlider);
-}
+//   const conditionsSlider = new Swiper($conditionsSlider, {
+//     mousewheel: {
+//       releaseOnEdges: true
+//     },
+//     pagination: {
+//       el: $conditionsSliderBullets,
+//       clickable: true,
+//       renderBullet: function(index, className) {
+//         return `
+//           <div class="${className} conditions__slider-bullet">
+
+//             <div class="conditions__slider-bullet-line"></div>
+//             <div class="conditions__slider-bullet-square">
+//               <div class="conditions__slider-bullet-text text__md text--uppercase">
+//                 <div class="conditions__slider-bullet-text-number">${index + 1}</div>
+//                 ${conditions[index].bullet}</div>
+//             </div>
+//           </div>
+//         `;
+//       }
+//     },
+//     on: {
+//       slideChangeTransitionEnd: function() {
+//         if (this.isBeginning || this.isEnd) {
+//           this.params.mousewheel.releaseOnEdges = true;
+//         } else {
+//           this.params.mousewheel.releaseOnEdges = false;
+//         }
+//       }
+//     },
+//     // slidesPerView: 1.2,
+//   });
+
+//   window.sliders.push(conditionsSlider);
+// }
