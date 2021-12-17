9c65ae24c4fb9ffa1fd1be1ed0144e16e9626836
Nikita <nikita.ableev@mail.ru>
2021-12-17 15:25:16 +0300

    Update mission slider

--- src/assets/scripts/mission-slider.js
+++ src/assets/scripts/mission-slider.js
@@ -1,18 +1,47 @@
 import Marquee3k from 'marquee3000';
 import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
 Swiper.use([Navigation, Pagination, Mousewheel]);
+import {mediaQuery} from './mediaQueries'
 
 export default function initMissionSlider() {
+
+  let sliderParams
+  let DELAY
+
+  if (mediaQuery.matches) {
+    DELAY = 1000
+    sliderParams = {
+      mousewheel: {
+        releaseOnEdges: true,
+        forceToAxis: true,
+        sensitivity: 1,
+        thresholdDelta: 40,
+      },
+      allowTouchMove: false,
+      speed: 1000,
+    }
+  } else {
+    DELAY = 500
+    sliderParams = {
+      mousewheel: {
+        releaseOnEdges: true,
+        forceToAxis: true,
+      },
+      speed: 750,
+    }
+  }
 
   Marquee3k.init();
 
   const $missionSlider = document.querySelector('[data-mission-slider]');
   const $missionSliderBullets = $missionSlider.querySelector('[data-mission-slider-bullets]');
   const $missionSliderCounter = $missionSlider.querySelector('[data-mission-slider-counter]');
+  const slider = $('[data-mission-slider]')
 
   const missionSlider = new Swiper($missionSlider, {
     direction: 'vertical',
-    allowTouchMove: false,
+    touchReleaseOnEdges: true,
+    ...sliderParams,
     on: {
       init: function() {
         const length = this.slides.length;
@@ -24,6 +53,14 @@ export default function initMissionSlider() {
         `;
       },
       slideChangeTransitionEnd: function() {
+
+        if (this.isBeginning || this.isEnd) {
+          this.params.mousewheel.releaseOnEdges = true;
+        } else {
+          this.params.mousewheel.releaseOnEdges = false;
+        }
+      },
+      slideChangeTransitionStart: function() {
         const length = this.slides.length;
         const index = this.activeIndex + 1;
 
@@ -31,12 +68,17 @@ export default function initMissionSlider() {
           <div class="mission__slider-counter-active">0${index}</div>
           <div class="mission__slider-counter-length">0${length}</div>
         `;
+      },
+      slideChange: function() {
+        const sliderOffset = slider.offset().top
+        const scrollPos = window.pageYOffset
 
-        // if (this.isBeginning || this.isEnd) {
-        //   this.params.mousewheel.releaseOnEdges = true;
-        // } else {
-        //   this.params.mousewheel.releaseOnEdges = false;
-        // }
+        if (sliderOffset !== scrollPos) {
+
+          $('html, body').animate({
+            scrollTop: sliderOffset 
+          }, DELAY);
+        }
       }
     },
     pagination: {
@@ -52,8 +94,6 @@ export default function initMissionSlider() {
       }
     }
   });
-
-  window.sliders.push(missionSlider);
-
   
+  window.sliders.push(missionSlider);
 }
