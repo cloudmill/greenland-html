db811c54361e546aef82dcf6d96d78d0b8134154
Nikita <nikita.ableev@mail.ru>
2021-12-20 12:30:44 +0300

    Update conditions-slider

--- src/assets/scripts/conditions-slider.js
+++ src/assets/scripts/conditions-slider.js
@@ -13,7 +13,7 @@ $(() => {
 
     const sections = $('.conditions__slider')
     const controller = new ScrollMagic.Controller()
-    const items = $('.pagina__item')
+    const items = $('.conditions-nav__item')
   
     const wipeAnimation = new TimelineMax()
       .fromTo(sections[0], 1, {x: "0"}, {x: "0%", ease: Linear.easeIn})
@@ -28,14 +28,20 @@ $(() => {
       .setPin("#conditions-container")
       .setTween(wipeAnimation)
       .addTo(controller);
+
+    
+    const state = {
+      boxActive: {background: '#65BA7F', borderColor: '#65BA7F', width: "12px", height: "12px"},
+      boxDefault: {background: 'transparent', borderColor: '#fff', width: "6px", height: "6px"},
+    }
     
     const wipe = new TimelineMax()
-      .fromTo(items.eq(0).find('.pagina__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
-      .to(items.eq(0).find('.pagina__box'), {background: 'transparent', borderColor: 'black'})
-      .to(items.eq(1).find('.pagina__box'), {background: 'green', borderColor: 'green'})
-      .fromTo(items.eq(1).find('.pagina__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
-      .to(items.eq(1).find('.pagina__box'), {background: 'transparent', borderColor: 'black'})
-      .to(items.eq(2).find('.pagina__box'), {background: 'green', borderColor: 'green'})
+      .fromTo(items.eq(0).find('.conditions-nav__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
+      .to(items.eq(0).find('.conditions-nav__box'), {...state.boxDefault})
+      .to(items.eq(1).find('.conditions-nav__box'), {...state.boxActive})
+      .fromTo(items.eq(1).find('.conditions-nav__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
+      .to(items.eq(1).find('.conditions-nav__box'), {...state.boxDefault})
+      .to(items.eq(2).find('.conditions-nav__box'), {...state.boxActive})
   
     const scene2 = new ScrollMagic.Scene({
       triggerElement: "#conditions-container",
