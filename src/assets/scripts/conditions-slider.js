29e81715bbd80817681becbf2ff1f5b32c65d49c
Nikita <nikita.ableev@mail.ru>
2021-12-27 12:37:22 +0300

    Update conditions-slider

--- src/assets/scripts/conditions-slider.js
+++ src/assets/scripts/conditions-slider.js
@@ -11,49 +11,68 @@ $(() => {
   
   if ($('#conditions-container').length) {
 
-    const state = {
-      boxActive: {background: '#65BA7F', borderColor: '#65BA7F', width: "12px", height: "12px"},
-      boxDefault: {background: 'transparent', borderColor: '#fff', width: "6px", height: "6px"},
-      titleFrom: {y: "-50%", opacity: 0},
-      titleTo: {y: "0%", opacity: 1},
-    }
-
     const sections = $('.conditions__slider')
     const controller = new ScrollMagic.Controller()
-    const items = $('.conditions-nav__item')
-  
-    const wipeAnimation = new TimelineMax()
-      .to(sections.eq(0).find('.conditions__slide-container'), 0.3, {...state.titleFrom, ease: Linear.easeIn})
-      .fromTo(sections[0], 0, {x: "0"}, {x: "0%", ease: Linear.easeIn})
-      .fromTo(sections[1], 1, {x: "100%"}, {x: "0%", ease: Linear.easeIn})
-      .fromTo(sections.eq(1).find('.conditions__slide-container'), 0.3, {...state.titleFrom}, {...state.titleTo, ease: Linear.easeIn})
-      .fromTo(sections[2], 1, {x: "100%"}, {x: "0%", ease: Linear.easeIn}) 
-      .fromTo(sections.eq(2).find('.conditions__slide-container'), 0.3, {...state.titleFrom}, {...state.titleTo, ease: Linear.easeIn})
-  
+    
+    const wipe = new TimelineMax()
     const scene = new ScrollMagic.Scene({
       triggerElement: "#conditions-container",
       triggerHook: "onLeave",
       duration: "500%"
     })
       .setPin("#conditions-container")
-      .setTween(wipeAnimation)
+      .setTween(wipe)
       .addTo(controller);
 
-    const wipe = new TimelineMax()
-      .fromTo(items.eq(0).find('.conditions-nav__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
-      .to(items.eq(0).find('.conditions-nav__box'), {...state.boxDefault})
-      .to(items.eq(1).find('.conditions-nav__box'), {...state.boxActive})
-      .fromTo(items.eq(1).find('.conditions-nav__line'), 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
-      .to(items.eq(1).find('.conditions-nav__box'), {...state.boxDefault})
-      .to(items.eq(2).find('.conditions-nav__box'), {...state.boxActive})
-  
+    
+    sections.each(function(index) {
+      const content = $(this).find('.conditions__slide-container')
+      const contentOut = TweenMax.to(content, 0.3, {y: "50%", opacity: 0, ease: Linear.easeIn})
+      const contetnIn = TweenMax.fromTo(content, 0.3, {y: "-50%", opacity: 0}, {y: "0%", opacity: 1})
+
+      if (index) {
+        const slideChange = TweenMax.fromTo($(this), 1, {x: "100%"}, {x: "0%", ease: Linear.easeIn})
+        
+        wipe.add(slideChange)
+        wipe.add(contetnIn)
+        if (index !== sections.length - 1) {
+          wipe.add(contentOut)
+        } 
+      } else {
+        wipe.add(contentOut)
+      }
+    })
+
+    const items = $('.conditions-nav__item')
+    const wipe2 = new TimelineMax()
     const scene2 = new ScrollMagic.Scene({
       triggerElement: "#conditions-container",
       triggerHook: "onLeave",
       duration: "500%"
     })
-      .setTween(wipe)
-    
-    controller.addScene(scene2)
+      .setTween(wipe2)
+      .addTo(controller)
+
+    items.each(function(i) {
+      const line = $(this).find('.conditions-nav__line')
+      const box = $(this).find('.conditions-nav__box')
+
+      const lineChange = TweenMax.fromTo(line, 1, {width: "0%"}, {width: "100%", ease: Linear.easeIn})
+      const boxDefault = TweenMax.to(box, 0.3, {background: 'transparent', borderColor: '#fff', width: "6px", height: "6px"})
+      const boxActive = TweenMax.to(box, 0.3, {background: '#65BA7F', borderColor: '#65BA7F', width: "12px", height: "12px"})
+
+      if (i) {
+        if (i === items.length - 1) {
+          wipe2.add(boxActive)
+        } else {
+          wipe2.add(boxActive)
+          wipe2.add(lineChange)
+          wipe2.add(boxDefault)
+        }
+      } else {
+        wipe2.add(lineChange)
+        wipe2.add(boxDefault)
+      }
+    })
   }
 })
