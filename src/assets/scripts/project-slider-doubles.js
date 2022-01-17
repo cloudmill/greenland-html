20136495efbf97f31f9c3f4afe6f71a9da4e01aa
Kirill Matveev <dejar@protonmail.com>
2022-01-17 11:57:42 +0300

    Fixes on the type page

---
+++ src/assets/scripts/project-slider-doubles.js
@@ -0,0 +1,53 @@
+import Swiper, {Navigation} from 'swiper';
+Swiper.use([Navigation]);
+
+$(window).on('load', () => {
+
+  const BREAKPOINT = 1280;
+
+  const $projectsSlider2 = document.querySelector('[data-projects-slider-2]');
+
+  const projectsSlider2 = new Swiper($projectsSlider2, {
+    slidesPerView: 'auto',
+    loop: true,
+
+    breakpoints: {
+      [BREAKPOINT]: {
+        centeredSlides: true,
+      },
+    },
+  });
+
+  $('[data-projects-slider-2-prev]').on('click', () => {
+    projectsSlider2.slidePrev();
+  });
+
+  $('[data-projects-slider-2-next]').on('click', () => {
+    projectsSlider2.slideNext()
+  });
+
+
+
+
+  const $projectsSlider3 = document.querySelector('[data-projects-slider-3]');
+
+  const projectsSlider3 = new Swiper($projectsSlider3, {
+    slidesPerView: 'auto',
+    loop: true,
+
+    breakpoints: {
+      [BREAKPOINT]: {
+        centeredSlides: true,
+      },
+    },
+  });
+
+  $('[data-projects-slider-3-prev]').on('click', () => {
+    projectsSlider3.slidePrev();
+  });
+
+  $('[data-projects-slider-3-next]').on('click', () => {
+    projectsSlider3.slideNext()
+  });
+
+});
