f3aa37fdf50095c3f5322a59d49b0533317a2f04
Kirill Matveev <dejar@protonmail.com>
2022-01-26 17:13:30 +0300

    Upd register form response

--- src/app.js
+++ src/app.js
@@ -49,47 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
-// модалка с первого экрана главной
-
-// {
-//   $(() => {
-//     const openBtn = $('.cober-modal-btn');
-
-//     if (openBtn.length) {
-//       const modalCover = $('.modal-cover');
-//       const main = $('.main');
-//       const closeBtn = $('.modal-cover-close');
-//       const body = $('.page-main');
-
-//       $(window).on('click', (event) => {
-
-//         if (!main.hasClass('modal-cover--open')) {
-
-//           if ( $(event.target).closest(openBtn).length ) {
-//             main.addClass('modal-cover--open');
-//             modalCover.addClass('modal-cover--active');
-//             body.addClass('overflow-hidden');
-//           }
-
-//         } else {
-
-//           if ( $(event.target).closest(closeBtn).length ) {
-//             main.removeClass('modal-cover--open');
-//             modalCover.removeClass('modal-cover--active');
-//             body.removeClass('overflow-hidden');
-//           }
-
-//           if (!$(event.target).closest(modalCover).length) {
-//             main.removeClass('modal-cover--open');
-//             modalCover.removeClass('modal-cover--active');
-//             body.removeClass('overflow-hidden');
-//           }
-//         }
-//       })
-//     }
-//   })
-// }
-
 // стили фокуса в инпутах форм
 
 {
